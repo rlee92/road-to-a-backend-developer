2ebced0a907e3081949a8f04046bd00edb6bfc6c
Ryan Lee <ryan.hg.lee@gmail.com>
2018-11-26 21:04:42 -0800

    [Refactor] List to Ajax

--- 2-JavaScript/fetch.js
+++ 2-JavaScript/fetch.js
@@ -1,7 +1,22 @@
 function fetchPage(name){
-    fetch(name).then(function(response){
-      response.text().then(function(text){
-        document.querySelector('article').innerHTML=text
-      })
+  fetch(name).then(function(response){
+    response.text().then(function(text){
+      document.querySelector('article').innerHTML=text
     })
-  }
+  })
+}
+fetch('list').then(function(response){
+  response.text().then(function(text){
+    var items = text.split(',');
+    var i = 0;
+    var tags = '';
+    while(i<items.length){
+      var item = items[i];
+      item = item.trim();
+      var tag = '<li><a href="#!'+item+'" onclick="fetchPage(\'contents/'+item+'\')">'+item+'</a></li>'
+      tags = tags + tag;
+      i++
+    }
+    document.querySelector('#nav').innerHTML = tags
+  })
+})
