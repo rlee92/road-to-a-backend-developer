98651e23c5ff628eb69d3b66a986e9ef1fb93555
Ryan Lee <ryan.hg.lee@gmail.com>
2018-12-10 21:21:17 -0800

    node.js Server init

--- 3-NodeJS/main.js
+++ 3-NodeJS/main.js
@@ -1,24 +1,22 @@
-const http = require('http')
-const url = require('url')
-const fs = require('fs')
+let http = require('http')
+let fs = require('fs')
+
 let app = http.createServer((request,response) => {
-    let _url = request.url
-    let queryData = url.parse(_url, true).query
-    console.log(queryData.id)
-    if(_url == '/'){
-      _url = '/index.html'
+    let url = request.url
+    if(request.url == '/'){
+      url = '/index.html'
     }
-    if(_url == '/favicon.ico'){
+    if(request.url == '/favicon.ico'){
       response.writeHead(404)
       response.end()
-      return
-
+      return;
     }
+    console.log(__dirname + url)
     response.writeHead(200)
-    // response.end(fs.readFileSync(__dirname + _url))
-    response.end(queryData.id)
+    response.end(fs.readFileSync(__dirname + url))
 
 })
+
 app.listen(3000, () => {
-  console.log('Server is running...')
+  console.log("Server is running...")
 })
