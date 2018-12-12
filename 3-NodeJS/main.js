b2b48571be6404f757e3f174303c1a56c9e440af
rlee92 <ryan.hg.lee@gmail.com>
2018-12-12 15:27:42 -0800

    Landing Page Handler

--- 3-NodeJS/main.js
+++ 3-NodeJS/main.js
@@ -8,33 +8,62 @@ let app = http.createServer((request,response) => {
   let queryData = url.parse(_url, true).query
   let title = queryData.id
 
-  if(pathName === '/') {
-    fs.readFile(`data/${queryData.id}`, 'utf8', (err, description) => {
-      let template = `
-      <!doctype html>
-      <html>
-      <head>
-      <title>WEB1 - ${title}</title>
-      <meta charset="utf-8">
-      </head>
-      <body>
-      <h1><a href="/">WEB</a></h1>
-      <ol>
-      <li><a href="/?id=HTML">HTML</a></li>
-      <li><a href="/?id=CSS">CSS</a></li>
-      <li><a href="/?id=JavaScript">JavaScript</a></li>
-      </ol>
-      <h2>${title}</h2>
-      <p> ${description}</p>
-      </body>
-      </html>
-      `
-      response.writeHead(200)
-      response.end(template)
-    })
+  if(pathName === '/'){
+    if(queryData.id === undefined){
+      fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
+        var title = 'Welcome'
+        var description = 'Hello, Node.js'
+        var template = `
+        <!doctype html>
+        <html>
+        <head>
+        <title>WEB1 - ${title}</title>
+        <meta charset="utf-8">
+        </head>
+        <body>
+        <h1><a href="/">WEB</a></h1>
+        <ul>
+        <li><a href="/?id=HTML">HTML</a></li>
+        <li><a href="/?id=CSS">CSS</a></li>
+        <li><a href="/?id=JavaScript">JavaScript</a></li>
+        </ul>
+        <h2>${title}</h2>
+        <p>${description}</p>
+        </body>
+        </html>
+        `
+        response.writeHead(200)
+        response.end(template)
+      })
+    } else {
+      fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
+        title = queryData.id
+        template = `
+        <!doctype html>
+        <html>
+        <head>
+        <title>WEB1 - ${title}</title>
+        <meta charset="utf-8">
+        </head>
+        <body>
+        <h1><a href="/">WEB</a></h1>
+        <ul>
+        <li><a href="/?id=HTML">HTML</a></li>
+        <li><a href="/?id=CSS">CSS</a></li>
+        <li><a href="/?id=JavaScript">JavaScript</a></li>
+        </ul>
+        <h2>${title}</h2>
+        <p>${description}</p>
+        </body>
+        </html>
+        `
+        response.writeHead(200)
+        response.end(template)
+      })
+    }
   } else {
-    response.writeHead(400)
-    response.end("Page Not Found")
+    response.writeHead(404)
+    response.end('Not found')
   }
 })
 
