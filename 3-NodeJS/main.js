c5355441da0da91a697a1441f6b1035d4911805c
rlee92 <ryan.hg.lee@gmail.com>
2018-12-12 10:39:58 -0800

    [Feature] Page Not Found Handler

--- 3-NodeJS/main.js
+++ 3-NodeJS/main.js
@@ -3,45 +3,39 @@ const fs = require('fs')
 const url = require('url')
 
 let app = http.createServer((request,response) => {
-    let _url = request.url
-    let queryData = url.parse(_url, true).query
-    let title = queryData.id
+  let _url = request.url
+  let pathName = url.parse(_url, true).pathname
+  let queryData = url.parse(_url, true).query
+  let title = queryData.id
 
-    if (_url == '/') {
-      title = 'Welcome'
-    }
-
-    if (_url == '/favicon.ico') {
-      response.writeHead(404)
-      response.end()
-      return;
-    }
-
-    response.writeHead(200)
-      fs.readFile(`data/${queryData.id}`, 'utf8', (err, description) => {
-      let temp = `
-        <!doctype html>
-        <html>
-        <head>
-          <title>WEB1 - ${title}</title>
-          <meta charset="utf-8">
-        </head>
-        <body>
-          <h1><a href="/">WEB</a></h1>
-          <ol>
-            <li><a href="/?id=HTML">HTML</a></li>
-            <li><a href="/?id=CSS">CSS</a></li>
-            <li><a href="/?id=JavaScript">JavaScript</a></li>
-          </ol>
-          <h2>${title}</h2>
-          <p> ${description}</p>
-        </body>
-        </html>
+  if(pathName === '/') {
+    fs.readFile(`data/${queryData.id}`, 'utf8', (err, description) => {
+      let template = `
+      <!doctype html>
+      <html>
+      <head>
+      <title>WEB1 - ${title}</title>
+      <meta charset="utf-8">
+      </head>
+      <body>
+      <h1><a href="/">WEB</a></h1>
+      <ol>
+      <li><a href="/?id=HTML">HTML</a></li>
+      <li><a href="/?id=CSS">CSS</a></li>
+      <li><a href="/?id=JavaScript">JavaScript</a></li>
+      </ol>
+      <h2>${title}</h2>
+      <p> ${description}</p>
+      </body>
+      </html>
       `
-      response.end(temp)
+      response.writeHead(200)
+      response.end(template)
     })
-    // response.end(fs.readFileSync(__dirname + _url))
-
+  } else {
+    response.writeHead(400)
+    response.end("Page Not Found")
+  }
 })
 
 app.listen(3000, () => {
