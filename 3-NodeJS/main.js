ed7dad165032d1e2c2f9c75431e575e20e8ee85b
rlee92 <ryan.hg.lee@gmail.com>
2018-12-13 14:50:53 -0800

    [Refactor] Clean up repetitive codes

--- 3-NodeJS/main.js
+++ 3-NodeJS/main.js
@@ -2,84 +2,69 @@ const http = require('http')
 const fs = require('fs')
 const url = require('url')
 
+let templateList = (fileList) => {
+  let list = '<ul>'
+  let i = 0
+  while(i < fileList.length) {
+    list += `<li><a href="/?id=${fileList[i]}">${fileList[i]}</a></li>`
+    i++
+  }
+  list += '</ul>'
+  return list
+}
+
+let templateHTML = (title, list, desc) => {
+  return `
+    <!doctype html>
+    <html>
+    <head>
+    <title>WEB1 - ${title}</title>
+    <meta charset="utf-8">
+    </head>
+    <body>
+    <h1><a href="/">WEB</a></h1>
+    ${list}
+    <h2>${title}</h2>
+    <p>${desc}</p>
+    </body>
+    </html>
+  `
+}
+
 let app = http.createServer((request,response) => {
   let _url = request.url
   let pathName = url.parse(_url, true).pathname
   let queryData = url.parse(_url, true).query
-  let title = queryData.id
 
   if(pathName === '/'){
     if(queryData.id === undefined){
-
       fs.readdir('./data', (error, fileList) => {
         let title = 'Welcome'
         let description = 'Hello, Node.js'
-        let list = '<ul>'
-        let i = 0;
-        while(i < fileList.length) {
-          list = list + `<li><a href="/?id=${fileList[i]}">${fileList[i]}</a></li>`
-          i++
-        }
-        list = list + '</ul>'
-
-        let template = `
-        <!doctype html>
-        <html>
-        <head>
-        <title>WEB1 - ${title}</title>
-        <meta charset="utf-8">
-        </head>
-        <body>
-        <h1><a href="/">WEB</a></h1>
-        ${list}
-        <h2>${title}</h2>
-        <p>${description}</p>
-        </body>
-        </html>
-        `
+        let list = templateList(fileList)
+        let template = templateHTML(title, list, description)
         response.writeHead(200)
         response.end(template)
       })
-
-
     } else {
-      fs.readdir('./data', function(error, fileList){
-        var title = 'Welcome';
-        var description = 'Hello, Node.js';
-        var list = '<ul>';
-        var i = 0;
-        while(i < fileList.length){
-          list = list + `<li><a href="/?id=${fileList[i]}">${fileList[i]}</a></li>`;
-          i = i + 1;
-        }
-        list = list+'</ul>';
-        fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
-          var title = queryData.id;
-          var template = `
-          <!doctype html>
-          <html>
-          <head>
-          <title>WEB1 - ${title}</title>
-          <meta charset="utf-8">
-          </head>
-          <body>
-          <h1><a href="/">WEB</a></h1>
-          ${list}
-          <h2>${title}</h2>
-          <p>${description}</p>
-          </body>
-          </html>
-          `;
-          response.writeHead(200);
-          response.end(template);
-        });
-      });
+      fs.readdir('./data', (error, fileList) => {
+        fs.readFile(`data/${queryData.id}`, 'utf8', (err, description) => {
+          let title = queryData.id
+          let list = templateList(fileList)
+          let template = templateHTML(title, list, description)
+          response.writeHead(200)
+          response.end(template)
+        })
+      })
     }
   } else {
-    response.writeHead(404);
-    response.end('Not found');
+    response.writeHead(404)
+    response.end('Page Not Found!')
   }
 })
+
+
+
 
   app.listen(3000, () => {
     console.log("Server is running...")
