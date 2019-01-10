4c6f49acea6b36a6a23f1524efcc0ef08f373f22
rlee92 <ryan.hg.lee@gmail.com>
2019-01-09 16:42:31 -0800

    Indentation fix

--- 3-NodeJS/main.js
+++ 3-NodeJS/main.js
@@ -33,18 +33,22 @@ let templateHTML = (title, list, desc, control) => {
   `
 }
 
+
 let app = http.createServer((request,response) => {
   let _url = request.url
   let pathName = url.parse(_url, true).pathname
   let queryData = url.parse(_url, true).query
 
-  if(pathName === '/'){
-    if(queryData.id === undefined){
+  // Base Path
+  if (pathName === '/') {
+    if (queryData.id === undefined) {
       fs.readdir('./data', (error, fileList) => {
         let title = 'Welcome'
         let description = 'Hello, Node.js'
         let list = templateList(fileList)
-        let template = templateHTML(title, list, description, `<a href="/create"> Create </a>`)
+        let createBtn = `<a href="/create"> Create </a>`
+        let template = templateHTML(title, list, description, createBtn)
+
         response.writeHead(200)
         response.end(template)
       })
@@ -53,88 +57,100 @@ let app = http.createServer((request,response) => {
         fs.readFile(`data/${queryData.id}`, 'utf8', (err, description) => {
           let title = queryData.id
           let list = templateList(fileList)
-          let template = templateHTML(title, list, description, `<a href="/create"> Create </a> <a href="/update?id=${title}"> Update </a>`)
+          let ctrlBtn = `<a href="/create"> Create </a> <a href="/update?id=${title}"> Update </a>`
+          let template = templateHTML(title, list, description, ctrlBtn)
+
           response.writeHead(200)
           response.end(template)
         })
       })
     }
+
+  // Create Page
   } else if (pathName === '/create') {
     fs.readdir('./data', (error, fileList) => {
       let title = 'WEB - Create'
       let list = templateList(fileList)
-      let template = templateHTML(title, list, `
-        <form action="/create_process" method="post">
+      let createForm = `
+      <form action="/create_process" method="post">
         <p><input type="text" name="title" placeholder="title"></p>
-        <p>
-        <textarea name="description" placeholder="description"></textarea>
-        </p>
-        <p>
-        <input type="submit">
-        </p>
-        </form>`, '')
+        <p><textarea name="description" placeholder="description"></textarea></p>
+        <p><input type="submit"></p>
+      </form>
+      `
+      let template = templateHTML(title, list, createForm, '')
+
+      response.writeHead(200)
+      response.end(template)
+    })
+
+  // Create Page Handler
+  } else if (pathName === '/create_process') {
+    let body = ''
+
+    request.on('data', (data) => {
+      body += data
+    })
+
+    request.on('end', _ => {
+      let post = qs.parse(body)
+      let title = post.title
+      let description = post.description
+
+      fs.writeFile(`data/${title}`, description, 'utf8', (err) => {
+        response.writeHead(302, {Location: `/?id=${title}`})
+        response.end()
+      })
+    })
+
+  // Update Page
+  } else if (pathName === '/update') {
+    fs.readdir('./data', (error, fileList) => {
+      fs.readFile(`data/${queryData.id}`, 'utf8', (err, description) => {
+        let title = queryData.id
+        let list = templateList(fileList)
+        let updateForm = `
+        <form action="/update_process" method="post">
+          <input type="hidden" name="id" value="${title}">
+          <p><input type="text" name="title" placeholder="title" value="${title}"></p>
+          <p><textarea name="description" placeholder="description" value=${description}></textarea></p>
+          <p><input type="submit"></p>
+        </form>
+        `
+        let ctrlBtn = `<a href="/create"> Create </a> <a href="/update?id=${title}"> Update </a>`
+        let template = templateHTML(title, list, updateForm, ctrlBtn)
+
         response.writeHead(200)
         response.end(template)
       })
-    } else if (pathName === '/create_process') {
-      let body = ''
-      request.on('data', (data) => {
-        body = body + data;
-      })
-      request.on('end', _ => {
-        let post = qs.parse(body);
-        let title = post.title;
-        let description = post.description
-        fs.writeFile(`data/${title}`, description, 'utf8', (err) => {
-          response.writeHead(302, {Location: `/?id=${title}`});
-          response.end();
-        })
-      })
-    } else if(pathName === '/update') {
-      fs.readdir('./data', (error, fileList) => {
-        fs.readFile(`data/${queryData.id}`, 'utf8', (err, description) => {
-          let title = queryData.id
-          let list = templateList(fileList)
-          let template = templateHTML(title, list,
-            `<form action="/update_process" method="post">
-            <input type="hidden" name="id" value="${title}">
-            <p><input type="text" name="title" placeholder="title" value="${title}"></p>
-            <p>
-            <textarea name="description" placeholder="description" value=${description}></textarea>
-            </p>
-            <p>
-            <input type="submit">
-            </p>
-            </form>
-            `
-          , `<a href="/create"> Create </a> <a href="/update?id=${title}"> Update </a>`)
-          response.writeHead(200)
-          response.end(template)
-        })
-      })
-    } else if(pathName === '/update_process') {
-      let body = ''
-      request.on('data', (data) => {
-        body = body + data;
-      })
-      request.on('end', _ => {
-        let post = qs.parse(body);
-        let title = post.title;
-        let id = post.id
-        let description = post.description
-        fs.rename(`data/${id}`, `data/${title}`, (err) => {
-          fs.writeFile(`data/${title}`, description, 'utf8', (err) => {
-            response.writeHead(302, {Location: `/?id=${title}`});
-            response.end();
-          })
-        })
-      })
-    } else {
-      response.writeHead(404)
-      response.end('Page Not Found!')
-    }
-  })
+    })
 
-  app.listen(3000, () => {
-    console.log("Server is running...")
-  })
+  // Update Page Handler
+  } else if (pathName === '/update_process') {
+    let body = ''
+
+    request.on('data', (data) => {
+      body += data
+    })
+    request.on('end', _ => {
+      let post = qs.parse(body)
+      let title = post.title
+      let id = post.id
+      let description = post.description
+
+      fs.rename(`data/${id}`, `data/${title}`, (err) => {
+        fs.writeFile(`data/${title}`, description, 'utf8', (err) => {
+          response.writeHead(302, {Location: `/?id=${title}`})
+          response.end()
+        })
+      })
+    })
+  } else {
+    response.writeHead(404)
+    response.end('Page Not Found!')
+  }
+})
+
+app.listen(3000, () => {
+  console.log("Server is running...")
+})
